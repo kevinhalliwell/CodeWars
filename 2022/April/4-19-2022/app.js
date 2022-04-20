dcaf74ce05bddde116838ede1d1ad1a7566ab932
Kevin Halliwell <godzirrasti@gmail.com>
2022-04-19 21:33:11 -0400

    update
    4-19-2022

---
+++ 2022/April/4-19-2022/app.js
@@ -0,0 +1,19 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+// P: iterate over an array and reorder the numbers from highest to lowest
+
+// R: Example : n=5 --> [5,4,3,2,1]
+
+// E:
+
+const reverseSeq = n => {
+    const result = [];
+    
+    for(let i = n; i > 0; i -= 1) {
+        result.push(i);
+    }
+    
+    return result;
+  };
