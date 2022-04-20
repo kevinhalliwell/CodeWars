b03eaff67e8e969b8d9dccb778029aee7c2a288d
Kevin Halliwell <godzirrasti@gmail.com>
2022-04-19 21:12:28 -0400

    upadtes
    4-18-2022

--- 2022/April/4-18-2022/app.js
+++ 2022/April/4-18-2022/app.js
@@ -5,9 +5,17 @@
 // name + " plays banjo" 
 // name + " does not play banjo"
 
-// P: enter a name both upper and lower case. 
+// P: enter a name both upper and lower case. check to see if the name starts with the letter r. if it does concatenate the name with plays banjo, if not concatenat with does not play banjo
+
+// R:  if it does concatenate the name with plays banjo, if not concatenate with does not play banjo
+
+// E: check the word using the starts with method
 
 function areYouPlayingBanjo(name) {
-    // Implement me
+	if (name.startsWith("R") === true || name.startsWith("r") === true) {
+		name = name + " plays banjo"
+	} else {
+		name = name + " does not play banjo"
+	}
     return name;
-  }
+}
