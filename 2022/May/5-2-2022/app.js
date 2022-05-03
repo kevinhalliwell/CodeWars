1d674f56a41bbe64c2fe4b9389050478d06f8327
kevinhalliwell <godzirrasti@gmail.com>
2022-05-03 17:00:49 -0400

    updates
    fixed 5/2 and added 5/3. codewars complete!

--- 2022/May/5-2-2022/app.js
+++ 2022/May/5-2-2022/app.js
@@ -6,4 +6,8 @@
 
 // R: return a string
 
-// E: I assume each vale will be one variable
+// E: I assume each value will be one variable
+
+function numberToString(num) {
+    return num.toString()
+  }
